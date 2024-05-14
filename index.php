4b8be1f265579ef696dce48931e1f871ee134ca3
NeilMagon <neil.magon97@gmail.com>
2024-05-14 16:05:33 +0200

    feat: add method

--- index.php
+++ index.php
@@ -15,10 +15,39 @@
             $this->genre = $_genre;
             $this->duration = $_duration;
         }
+
+        public function getFullMovie(){
+            $full_movie = $this->name . ', ' . $this->director . ', ' . $this->year;
+            return $full_movie;
+        }
+        public function getMovieInfo(){
+            $movie_info = $this->rating . ', ' . $this->genre . ', ' . $this->duration;
+            return $movie_info;
+        }
     }
 
-    $movie_1 = new Movie('Joker','Todd Philips', '2019', '8.4', 'Thriller', '121 min');
-    $movie_2 = new Movie('Bohemian Rhapsody','Bryan Singer', '2018', '8.1', 'Biografico', '134 min');
+    $movie_1 = new Movie('Joker','Todd Philips', 2019, '8.4', 'Thriller', '121 min');
+    $movie_2 = new Movie('Bohemian Rhapsody','Bryan Singer', 2018, '8.1', 'Biografico', '134 min');
     var_dump($movie_1);
-    var_dump($movie_2)
+    var_dump($movie_2);
+
+    $movies = [
+        $movie_1,
+        $movie_2
+    ]
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <?php foreach($movies as $movie){ ?>
+        <h1><?php echo $movie->getFullMovie(); ?></h1>
+        <h2><?php echo $movie->getMovieInfo(); ?></h2>
+    <?php } ?>
+</body>
+</html>
